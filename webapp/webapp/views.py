74c8bd4625e9d42d714331f38990f69a27d329f2
Andres Vargas <vargasa@cua.edu>
2019-04-15 17:00:04 +0200

    django skeleton

---
+++ webapp/webapp/views.py
@@ -0,0 +1,12 @@
+from django.http import JsonResponse, HttpResponseBadRequest
+from AIErrorHandling.models import Predictor
+
+def predict(request):
+    try:
+        response = Predictor( **{
+                "wf" : request.POST.wf,
+                "tsk" : request.POST.tsk,
+                })
+        return JsonResponse(response)
+    else:
+        return HttpResponseBadRequest()
